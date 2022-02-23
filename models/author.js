7b33f61b2955a49777960f31f06d57b45fd310b3
Timothy Ponce <timothyjponce@gmail.com>
2022-02-22 22:35:20 -0800

    Add controllers and view for authors list

--- models/author.js
+++ models/author.js
@@ -1,3 +1,5 @@
+const { DateTime } = require("luxon");
+
 var mongoose = require("mongoose");
 
 var Schema = mongoose.Schema;
@@ -39,6 +41,20 @@ AuthorSchema.virtual("lifespan").get(function () {
 // Virtual for author's URL
 AuthorSchema.virtual("url").get(function () {
   return "/catalog/author/" + this._id;
+});
+
+// Virtual for author's date of birth
+AuthorSchema.virtual("date_of_birth_formatted").get(function () {
+  return this.date_of_birth
+    ? DateTime.fromJSDate(this.date_of_birth).toLocaleString(DateTime.DATE_MED)
+    : "";
+});
+
+//Virtual for author's date of death
+AuthorSchema.virtual("date_of_death_formatted").get(function () {
+  return this.date_of_death
+    ? DateTime.fromJSDate(this.date_of_death).toLocaleString(DateTime.DATE_MED)
+    : "";
 });
 
 // Export model
