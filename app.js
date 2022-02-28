11a58ecf6f02024fb6f7863351bfa7290d86c5ef
Timothy Ponce <timothyjponce@gmail.com>
2022-02-28 11:17:44 -0800

    Install compression and helmet dependencies

--- app.js
+++ app.js
@@ -7,11 +7,15 @@ var logger = require("morgan");
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 var catalogRouter = require("./routes/catalog"); //Import routes for "catalog" area of site
+var compression = require("compression");
+var helmet = require("helmet");
 
+// Create the Express application object
 var app = express();
 
 //Set up mongoose connection
 var mongoose = require("mongoose");
+const req = require("express/lib/request");
 var mongoDB =
   "mongodb+srv://tim:gQhagcc8Hhm8K7GX@cluster0.ctxjn.mongodb.net/local_library?retryWrites=true&w=majority";
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -22,6 +26,8 @@ db.on("error", console.error.bind(console, "MongoDB connection error:"));
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+app.use(compression());
+app.use(helmet());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
