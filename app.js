306eeb53a7d2d7b4a072ba1b9299432044bfa9ca
Timothy Ponce <timothyjponce@gmail.com>
2022-02-28 11:22:43 -0800

    Added files and changes required for deployment to heroku

--- app.js
+++ app.js
@@ -16,8 +16,9 @@ var app = express();
 //Set up mongoose connection
 var mongoose = require("mongoose");
 const req = require("express/lib/request");
-var mongoDB =
+var dev_db_url =
   "mongodb+srv://tim:gQhagcc8Hhm8K7GX@cluster0.ctxjn.mongodb.net/local_library?retryWrites=true&w=majority";
+var mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 var db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
