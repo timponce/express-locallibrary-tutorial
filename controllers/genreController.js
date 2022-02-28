db1e91479971d57c8236af2a1fcaced3e0641dc5
Timothy Ponce <timothyjponce@gmail.com>
2022-02-28 11:00:09 -0800

    Add controller and view to update genre

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -170,11 +170,57 @@ exports.genre_delete_post = function (req, res, next) {
 };
 
 // Display Genre update form on GET.
-exports.genre_update_get = function (req, res) {
-  res.send("NOT IMPLEMENTED: Genre update GET");
+exports.genre_update_get = function (req, res, next) {
+  Genre.findById(req.params.id).exec(function (err, genre) {
+    if (err) {
+      return next(err);
+    }
+    // Successful, so render.
+    res.render("genre_form", {
+      title: "Update Genre",
+      genre: genre,
+    });
+  });
 };
 
 // Handle Genre update on POST.
-exports.genre_update_post = function (req, res) {
-  res.send("NOT IMPLEMENTED: Genre update POST");
-};
+exports.genre_update_post = [
+  // Validate and santize fields.
+  body("name", "Genre name required").trim().isLength({ min: 1 }).escape(),
+
+  // Process request after validation and sanitization.
+  (req, res, next) => {
+    // Extract the validation errors from a request.
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      // There are errors. Render form again with sanitized values/error messages.
+      res.render("genre_form", {
+        title: "Update Genre",
+        genre: req.body,
+        errors: errors.array(),
+      });
+      return;
+    } else {
+      // Data from form is valid.
+
+      // Create a Genre object with escaped and trimmed data and old id.
+      var genre = new Genre({
+        name: req.body.name,
+        _id: req.params.id, //This is required, or a new ID will be assigned!
+      });
+      Genre.findByIdAndUpdate(
+        req.params.id,
+        genre,
+        {},
+        function (err, thegenre) {
+          if (err) {
+            return next(err);
+          }
+          // Successful - redirect to genre record.
+          res.redirect(thegenre.url);
+        }
+      );
+    }
+  },
+];
